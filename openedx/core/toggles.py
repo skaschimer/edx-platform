c338f090b078892d89b06942c289daca60b48d02
Rodrigo Mendez <117670175+rodmgwgu@users.noreply.github.com>
2026-02-09 10:06:01 -0600

    feat: Add Waffle flag for AuthZ for Course Authoring (#37985)

--- openedx/core/toggles.py
+++ openedx/core/toggles.py
@@ -3,6 +3,8 @@ Feature toggles used across the platform. Toggles should only be added to this m
 for them. Generally speaking, they should be added to the most appropriate app or repo.
 """
 from edx_toggles.toggles import SettingToggle
+from openedx.core.djangoapps.waffle_utils import CourseWaffleFlag
+
 
 # .. toggle_name: ENTRANCE_EXAMS
 # .. toggle_implementation: SettingToggle
@@ -15,3 +17,14 @@ from edx_toggles.toggles import SettingToggle
 ENTRANCE_EXAMS = SettingToggle(
     "ENTRANCE_EXAMS", default=False, module_name=__name__
 )
+
+# .. toggle_name: authz.enable_course_authoring
+# .. toggle_implementation: CourseWaffleFlag
+# .. toggle_default: False
+# .. toggle_description: This toggle will enable the new openedx-authz authorization engine for course authoring.
+# .. toggle_warning: Enabling this toggle will trigger a data migration to move role assignations between the legacy and the openedx-authz system.
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2026-02-05
+# .. toggle_target_removal_date: 2027-06-09
+# .. toggle_tickets: https://github.com/openedx/openedx-platform/issues/37927
+AUTHZ_COURSE_AUTHORING_FLAG = CourseWaffleFlag('authz.enable_course_authoring', __name__)
